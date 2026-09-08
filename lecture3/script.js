092fb6695a233e72c9a2d04b15bf8c41854ae392
yashsingla1976-debug <yash.singla1976@gmail.com>
2026-09-08 19:05:25 +0530

    first commit

---
+++ lecture3/script.js
@@ -0,0 +1,22 @@
+// const math=require("./math.js"); //local module
+// const os=require("os"); //core mpodule
+// const express=require("express"); //third party module
+
+// import { add, subtract } from "./math.js"; //local module
+
+
+// console.log(add(5, 10));
+// console.log(subtract(10, 5));
+// console.log(os.cpus().length);
+
+
+
+
+// console.log(math.add(5, 10));
+// console.log(math.subtract(10, 5));
+
+// console.log("Hello Students!!")
+
+// const math=require("./math.js");
+
+// console.log(math.add(5, 10));
