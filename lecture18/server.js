aada42cfec9b22926d14d183b6f8fe273a905ac2
yashsingla1976-debug <yash.singla1976@gmail.com>
2026-09-25 13:46:43 +0530

    first commit

--- lecture18/server.js
+++ lecture18/server.js
@@ -1,36 +1,49 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res, next) => {
-    try{
+// Home route
+app.get("/", (req, res, next) => {
+    try {
         throw new Error("Something went wrong!!!!!!!!!!");
-    }catch(error){
-        // res.status(500).json({success:false, message:"Something went wrong"});
-        next(error);
-    }
-})
-
-app.get("/:id",(req,res,next)=>{
-    const id=req.params.id;
-    try{
-        if(id==="1234"){
-            res.send("Valid id");
-        }else{
-            throw new Error("Invalid id");
-        }
-    }catch(error){
-        // res.status(400).json({success:false, message:"invalid Id"});
+    } catch (error) {
         next(error);
     }
 });
 
-app.use((err,req,res,next)=>{  //error  middleware
-    res.status(500).json({success:false, message:err.message});
-})
+// ID route
+app.get("/:id", (req, res, next) => {
+    const id = req.params.id;
 
-app.use((req,res)=>{   //invalid route middleware
-    res.status(404).json({success:false, message:"Page not found"});
-})
+    try {
+        if (id === "1234") {
+            res.send("Valid id");
+        } else {
+            throw new Error("Invalid id");
+        }
+    } catch (error) {
+        next(error);
+    }
+});
 
-app.listen(PORT, () => console.log(`Server is running `));
+// Error handling middleware
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        success: false,
+        message: err.message
+    });
+});
+
+// 404 middleware
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Page not found"
+    });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
